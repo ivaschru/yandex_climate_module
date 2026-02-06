89dbacc3ecc3923a6d7271b377b19b7a10b7cf22
ivaschru <83538648+ivaschru@users.noreply.github.com>
2026-02-06 13:02:15 +0300

    Create const.py

---
+++ custom_components/yandex_climate_modules/const.py
@@ -0,0 +1,16 @@
+DOMAIN = "yandex_climate_modules"
+PLATFORMS = ["sensor"]
+
+CONF_TOKEN = "token"
+CONF_DEVICE_IDS = "device_ids"
+
+DEFAULT_UPDATE_INTERVAL = 120  # seconds
+
+YANDEX_IOT_BASE = "https://api.iot.yandex.net/v1.0"
+
+# Property instances we care about
+INST_TEMPERATURE = "temperature"
+INST_HUMIDITY = "humidity"
+INST_CO2 = "co2_level"
+
+CLIMATE_INSTANCES = {INST_TEMPERATURE, INST_HUMIDITY, INST_CO2}
